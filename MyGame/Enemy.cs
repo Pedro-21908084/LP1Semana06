756d69952814d721c16230df5e8f83e1eb235efe
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-30 18:44:23 +0100

    create variables and constrocter for Enemy class

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -2,6 +2,16 @@ namespace MyGame
 {
     public class Enemy
     {
-        
+        private string name;
+        private float health;
+        private float shield;
+
+        //controctor
+        public Enemy(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
     }
 }
