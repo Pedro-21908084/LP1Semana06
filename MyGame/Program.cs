61723b8af4f9ffc9ca113194edcdf99a3e62cc52
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-30 19:15:21 +0100

    Create an enemy array with the size given

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -8,12 +8,18 @@ namespace MyGame
         {
             //variables
             int enemyNumber;
+            Enemy[] enemys;
+            string enemyName, everyName;
 
             //ask enemy numbers
             Console.Write("Number of enemys: ");
             enemyNumber = int.Parse(Console.ReadLine());
+
             //create array
+            enemys = new Enemy[enemyNumber];
+
             //ask names of enemys
+
             //print names
             Console.WriteLine($"You chose {enemyNumber} enemys.");
         }
