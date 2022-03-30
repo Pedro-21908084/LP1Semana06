211fbd7e812f3586f636b5f6d815b333d7618e7c
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-30 19:04:38 +0100

    Progam asks how many enemys user wants

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -7,10 +7,15 @@ namespace MyGame
         static void Main(string[] args)
         {
             //variables
+            int enemyNumber;
+
             //ask enemy numbers
+            Console.Write("Number of enemys: ");
+            enemyNumber = int.Parse(Console.ReadLine());
             //create array
             //ask names of enemys
             //print names
+            Console.WriteLine($"You chose {enemyNumber} enemys.");
         }
     }
 }
